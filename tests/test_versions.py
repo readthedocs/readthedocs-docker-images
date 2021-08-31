f68c7f16a266ffbbc3c1c5411336bcc9422154a1
Manuel Kaufmann <humitos@gmail.com>
2021-08-31 12:26:38 +0200

    Typo

--- tests/test_versions.py
+++ tests/test_versions.py
@@ -8,10 +8,10 @@ from .utils import run_command_in_container
     'command,expected_output',
     [
         ('asdf version', 'v0.8.1-a1ef92a'),
-        ('asdf plugin list', '\n'.join(['golang', 'nodejs', 'python', 'rust']),
+        ('asdf plugin list', '\n'.join(['golang', 'nodejs', 'python', 'rust'])),
         ('git --version', 'git version 2.25.1'),
     ]
 )
-def test_command_versions_image_master(command, expected_output):
-    cmd_output = run_command_in_container('readthedocs/build:master', command)
+def test_command_versions_image_ubuntu20(command, expected_output):
+    cmd_output = run_command_in_container('readthedocs/build:ubuntu20', command)
     assert cmd_output == expected_output
